70c914b926a10ed75dfd8ae4181d1ffbe66a1179
Brandon Brown <105027816+Bbrown22@users.noreply.github.com>
2024-04-05 21:09:53 -0700

    html needs work still

--- src/main/java/com/codercampus/Assignment11/service/TransactionService.java
+++ src/main/java/com/codercampus/Assignment11/service/TransactionService.java
@@ -1,5 +1,9 @@
 package com.codercampus.Assignment11.service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +12,20 @@ import com.codercampus.Assignment11.repository.TransactionRepository;
 
 @Service
 public class TransactionService {
-	@Autowired
-	private TransactionRepository transRepo;
-	
-	public Transaction getId(Long id) {
-		return transRepo.findById(id);
-	}
+	 private final TransactionRepository transactionRepository;
+
+	    @Autowired
+	    public TransactionService(TransactionRepository transactionRepository) {
+	        this.transactionRepository = transactionRepository;
+	    }
+
+	    public List<Transaction> findAll() {
+	        return transactionRepository.findAll().stream()
+	                                    .sorted(Comparator.comparing(Transaction::getDate))
+	                                    .collect(Collectors.toList());
+	    }
+	    public Transaction findById(Long id) {
+	        return transactionRepository.findById(id);
+	    }
 
 }
--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -7,28 +7,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.codercampus.Assignment11.domain.Transaction;
-import com.codercampus.Assignment11.repository.TransactionRepository;
+import com.codercampus.Assignment11.service.TransactionService;
 
 @Controller
 public class TransactionController {
 	
-	private final TransactionRepository transactionRepository;
+	private final TransactionService transactionService;
 	
 	@Autowired
-	public TransactionController(TransactionRepository transactionRepository) {
-		this.transactionRepository = transactionRepository;
+	public TransactionController(TransactionService transactionService) {
+		this.transactionService = transactionService;
 	}
 	
 	@GetMapping("/transaction")
 	public String getTransactions(ModelMap model) {
-		List<Transaction> transactions = transactionRepository.findAll();
+		List<Transaction> transactions = transactionService.findAll();
 		transactions.sort(Comparator.comparing(Transaction::getDate));
 		model.addAttribute("transactions",transactions);
 
 		return "transactions";
 	}
+	 @GetMapping("/transactions/{transactionId}")
+	    public String getTransaction(@PathVariable Long transactionId, ModelMap model) {
+	        Transaction transaction = transactionService.findById(transactionId);
+	        model.addAttribute("transaction", transaction);
+	        return "transaction";
+	    }
 	
 	
 	
